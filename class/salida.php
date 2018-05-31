fee47645473ee43fa05e12721c9e1f25206b7279
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2018-05-30 20:51:59 -0400

    Class Salida

---
+++ class/salida.php
@@ -0,0 +1,11 @@
+<?php
+include_once("bd.php");
+class salida extends bd{
+	var $tabla="salida";
+
+	function cantidadSalidas($codmaterial,$codalmacen){
+        $this->campos=array('sum(cantidad) as SalidaTotal');
+        return $this->mostrarTodoRegistro("codmaterial=$codmaterial and codalmacen=$codalmacen",1,"");
+    }
+}
+?>
